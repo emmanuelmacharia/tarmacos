728f7fc81464a05e98cddde388ed0d55a4157dfd
emmanuelmacharia <samuelmarsha@outlook.com>
2026-03-13 20:36:26 +0300

    wraps the app in clerk provider; and rudimentary navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { ClerkProvider } from 'svelte-clerk';
+	import Homepagenav from '$lib/components/homepagenav.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ClerkProvider>
 <main>
+<Homepagenav />
 	{@render children()}
 </main>
+</ClerkProvider>
