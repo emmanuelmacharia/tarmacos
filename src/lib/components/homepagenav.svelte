045ef8a18df79621b4836684fc32d391bec1e1da
emmanuelmacharia <samuelmarsha@outlook.com>
2026-03-13 20:37:23 +0300

    adds rudimentary nav; and configures signin button from clerk

---
+++ src/lib/components/homepagenav.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Show, SignInButton, UserButton } from 'svelte-clerk';
+</script>
+
+
+	<header class="absolute top-0 right-0 p-6 flex gap-4">
+		<Show when="signed-out">
+			<SignInButton />
+		</Show>
+		<Show when="signed-in">
+			<UserButton />
+		</Show>
+	</header>
