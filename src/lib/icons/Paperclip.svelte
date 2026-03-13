dccfc9873a610687dada5db008f0d1dd23318abc
emmanuelmacharia <samuelmarsha@outlook.com>
2026-03-13 11:22:20 +0300

    icon sets for the svelte app. we cant use a lib because of compatibility issues

---
+++ src/lib/icons/Paperclip.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Icon from './Icon.svelte';
+
+	type Props = {
+		class?: string;
+		size?: number | string;
+		strokeWidth?: number | string;
+		ariaLabel?: string;
+	};
+
+	let { class: className = '', size = 20, strokeWidth = 2, ariaLabel }: Props = $props();
+</script>
+
+<Icon class={className} {size} {strokeWidth} {ariaLabel}>
+   <path d="m16 6l-8.414 8.586a2 2 0 0 0 2.829 2.829l8.414-8.586a4 4 0 1 0-5.657-5.657l-8.379 8.551a6 6 0 1 0 8.485 8.485l8.379-8.551"/>
+</Icon>
---
+++ src/lib/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as Icon} from './Icon.svelte';
+export { default as Paperclip } from './Paperclip.svelte'
+export { default as Sparkles } from './Sparkles.svelte'
