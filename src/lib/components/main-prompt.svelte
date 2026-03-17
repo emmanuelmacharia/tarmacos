6911947a4ee5555e5b400ba27aae5fa8941c9372
emmanuelmacharia <samuelmarsha@outlook.com>
2026-03-17 21:10:35 +0300

    wrap prompt in form

--- src/lib/components/main-prompt.svelte
+++ src/lib/components/main-prompt.svelte
@@ -11,11 +11,13 @@
 	<div
 		class="relative w-full rounded-2xl border border-primary/30 bg-background shadow-lg transition-colors focus-within:border-primary focus-within:ring-4 focus-within:ring-primary/10 hover:border-border"
 	>
+	<form action="">
 		<textarea
 			rows="2"
 			class="w-full resize-none bg-transparent p-4 text-lg leading-relaxed outline-none placeholder:text-muted-foreground/50 md:text-xl"
 			placeholder="Paste a Job description of the role..."
 		></textarea>
+		
 		<div class="mt-2 flex items-center justify-between p-2">
 			<div
 				class="flex cursor-pointer items-center gap-2 rounded-lg px-3 py-2 text-sm font-medium text-muted-foreground transition hover:bg-muted/50 hover:text-foreground"
@@ -28,6 +30,7 @@
 				Tailor resume
 			</Button>
 		</div>
+	</form>
 	</div>
 	<div class="mt-10 items-center justify-center sm:gap-2 md:gap-4">
 		<button
