2cd185e3664104b12c07be3a698f488ec5d7cd14
emmanuelmacharia <samuelmarsha@outlook.com>
2026-03-17 09:35:21 +0300

    creates dashboard button to route the user into the dashboard when logged in. We can think of the ux on login later

--- src/lib/components/homepagenav.svelte
+++ src/lib/components/homepagenav.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Show, SignInButton, UserButton } from 'svelte-clerk';
+	import Button from './ui/button/button.svelte';
 </script>
 
 <header class="pointer-events-auto absolute top-0 right-0 z-50 flex gap-4 p-6">
@@ -8,5 +9,6 @@
 	</Show>
 	<Show when="signed-in">
 		<UserButton />
+		<Button class="rounded-none bg-primary">Dashboard</Button>
 	</Show>
 </header>
