7674095debfbd02bf2863669c5a51b4fd660cc50
emmanuelmacharia <samuelmarsha@outlook.com>
2026-03-17 11:21:25 +0300

    initial commit for nav

--- src/lib/components/homepagenav.svelte
+++ src/lib/components/homepagenav.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Show, SignInButton, UserButton } from 'svelte-clerk';
 	import Button from './ui/button/button.svelte';
+	import { SquareArrowRightEnter } from '@lucide/svelte';
 </script>
 
 <header class="pointer-events-auto absolute top-0 right-0 z-50 flex gap-4 p-6">
@@ -9,6 +10,9 @@
 	</Show>
 	<Show when="signed-in">
 		<UserButton />
-		<Button class="rounded-none bg-primary">Dashboard</Button>
+		<Button class="flex gap-4 rounded-none bg-primary">
+			Dashboard
+			<SquareArrowRightEnter />
+		</Button>
 	</Show>
 </header>
