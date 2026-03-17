315aa1df2d4d047cf631e13a9ac5d56ed2e64781
emmanuelmacharia <samuelmarsha@outlook.com>
2026-03-17 16:23:36 +0300

    fix homepage nav

--- src/lib/components/homepagenav.svelte
+++ src/lib/components/homepagenav.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { Show, SignInButton, UserButton } from 'svelte-clerk';
-	import Button from './ui/button/button.svelte';
-	import { SquareArrowRight } from '@lucide/svelte';
 </script>
 
 <header class="pointer-events-auto absolute top-0 right-0 z-50 flex gap-4 p-6">
@@ -10,9 +8,5 @@
 	</Show>
 	<Show when="signed-in">
 		<UserButton />
-		<Button class="flex gap-4 rounded-none bg-primary">
-			Dashboard
-			<SquareArrowRight />
-		</Button>
 	</Show>
 </header>
