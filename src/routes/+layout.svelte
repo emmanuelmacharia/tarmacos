80c92b1c26ddef7c323ad068d981e98785cedb35
emmanuelmacharia <samuelmarsha@outlook.com>
2026-04-07 16:17:30 +0300

    adds sonner to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,14 @@
 	import { PUBLIC_CONVEX_URL } from '$env/static/public';
 	import { setupConvex } from 'convex-svelte';
 	import ConvexClerkProvider from '$lib/providers/ConvexClerkProvider.svelte';
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
 
 	setupConvex(PUBLIC_CONVEX_URL);
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<Toaster />
 <ClerkProvider>
 	<ConvexClerkProvider />
 	<main>
