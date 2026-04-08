9a0633f1a5617a8e28e41e1443d2365235a5aacc
emmanuelmacharia <samuelmarsha@outlook.com>
2026-04-08 08:19:01 +0300

    introduced a load function to load users profiles on nav

---
+++ src/routes/(app)/dashboard/+layout.server.ts
@@ -0,0 +1,28 @@
+import { ConvexHttpClient } from 'convex/browser';
+import type { LayoutServerLoad } from './$types';
+import { api } from '../../../convex/_generated/api';
+import { PUBLIC_CONVEX_URL } from '$env/static/public';
+import { redirect } from '@sveltejs/kit';
+import { parseConvexMessage } from '$lib/utils/errorHandler';
+
+const client = new ConvexHttpClient(PUBLIC_CONVEX_URL);
+
+export const load: LayoutServerLoad = async (event) => {
+	const auth = await event.locals.auth();
+	const token = await auth.getToken({ template: 'convex' });
+
+	if (token) {
+		client.setAuth(token);
+	}
+	try {
+		const profiles = await client.query(api.user.profiles.fetchUserProfiles);
+		return { profiles };
+	} catch (error) {
+		if (error instanceof Error) {
+			const errorObj = parseConvexMessage(error.message);
+			if (errorObj?.code === 'UNAUTHORIZED' || errorObj?.code === 'FORBIDDEN') {
+				throw redirect(303, '/');
+			}
+		}
+	}
+};
