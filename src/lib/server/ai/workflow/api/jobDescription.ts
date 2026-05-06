2c7114b074fb84dd500e5255f51034727f566126
emmanuelmacharia <samuelmarsha@outlook.com>
2026-05-06 13:19:21 +0300

    adds persist document for jd

--- src/lib/server/ai/workflow/api/jobDescription.ts
+++ src/lib/server/ai/workflow/api/jobDescription.ts
@@ -2,12 +2,46 @@
 
 import type { ConvexHttpClient } from 'convex/browser';
 import { api } from '../../../../../convex/_generated/api';
+import type { Id } from '../../../../../convex/_generated/dataModel';
+
+type AllowedFileTypes = 'pdf' | 'docx' | 'markdown' | 'txt' | 'json';
 
 export async function createFileFromJD(
 	convex: ConvexHttpClient,
-	args: { text: string; filename: string }
+	args: { text: string; filename: string; profileId?: Id<'profiles'> }
 ) {
-	console.log(convex);
 	const file = await convex.action(api.runs.runDocuments.saveTextFile, { ...args });
-	return file;
+
+	const persistedDoc = await persistJDtoDB(convex, {
+		profileId: args.profileId,
+		fileName: file.filename,
+		fileSize: file.size,
+		storageId: file.storageId,
+		format: 'txt' as AllowedFileTypes,
+		mimeType: file.mimeType
+	});
+	return { ...file, documentId: persistedDoc.data };
+}
+
+export async function persistJDtoDB(
+	convex: ConvexHttpClient,
+	args: {
+		profileId?: Id<'profiles'>;
+		fileName: string;
+		fileSize: number;
+		storageId: Id<'_storage'>;
+		format: AllowedFileTypes;
+		mimeType: string;
+	}
+) {
+	const payload = {
+		profileId: args.profileId ?? undefined,
+		name: args.fileName,
+		fileSize: args.fileSize,
+		storageId: args.storageId,
+		documentFormat: args.format,
+		mimeType: args.mimeType,
+		documentType: 'job_description' as const
+	};
+	return await convex.mutation(api.documents.upload.registerUpload, payload);
 }
